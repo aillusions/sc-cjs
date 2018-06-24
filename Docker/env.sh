0f9db738332e38e5fc38d4cd2edb750ba8964ca1
Alex Zalizniak <alex@xplace.com>
2018-06-24 16:55:03 +0300

    Docker - misc

--- Docker/env.sh
+++ Docker/env.sh
@@ -5,6 +5,16 @@
 export SCC_APP_WEB_HOST_PORT=docker.for.mac.localhost:8090
 
 
+cd ./nginx_common
+docker build -t openresty-bundle:1.13.6.1-0 .
+cd ..
+
+
+cd ./nginx
+docker build -t scc-nginx:1.13.6.1-0 .
+cd ..
+
+
 docker-compose down
 docker-compose build
 docker-compose up mysql-scc-dev scc-nginx
