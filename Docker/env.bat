9da513a218dc70b4567ba0a394786499e9fb614c
Alex Zalizniak <alex@xplace.com>
2018-06-24 16:55:34 +0300

    Docker - misc

--- Docker/env.bat
+++ Docker/env.bat
@@ -1,6 +1,16 @@
 
 set SCC_APP_WEB_HOST_PORT=docker.for.win.localhost:8090
 
+
+cd nginx_common
+docker build -t openresty-bundle:1.13.6.1-0 .
+cd ..
+
+
+cd nginx
+docker build -t scc-nginx:1.13.6.1-0 .
+cd ..
+
 docker-compose down
 docker-compose up mysql-scc-dev scc-nginx
 
